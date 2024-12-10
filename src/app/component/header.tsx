89487cd5aad0f24a9dff8f14053a7affa6c28da4
Saleha Khan <161796561+Ibrahimsabir@users.noreply.github.com>
2024-12-10 08:38:15 -0800

    Update header.tsx

--- src/app/component/header.tsx
+++ src/app/component/header.tsx
@@ -1,26 +1,24 @@
-import React from 'react'
+import React from 'react';
 import Link from 'next/link';
-import {Rye} from '@next/font/google';
+import { Rye } from '@next/font/google';
 
-const rye=Rye({
-    subsets:["latin"],
-    weight:["400"]
-})
+const rye = Rye({
+  subsets: ['latin'],
+  weight: ['400'],
+});
+
 const Header = () => {
   return (
     <div className={`${rye.className}`}>
-        <div className="absolute w-[1920px] h-[134px] left-0 top-0 bg-[#A29875] text-white">
-        <h1
-          className="absolute w-[439px] h-[94px] left-[calc(50%-439px/2-704.5px)] top-[20px] 
-         text-[75px] leading-[94px] font-normal"
-        >
+      <div className="w-full h-[80px] md:h-[134px] bg-[#A29875] text-white flex items-center px-4 md:px-8">
+        <h1 className="text-[36px] md:text-[75px] leading-tight font-normal">
           <Link href="/">
             <span>MANZARRI</span>
           </Link>
         </h1>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
